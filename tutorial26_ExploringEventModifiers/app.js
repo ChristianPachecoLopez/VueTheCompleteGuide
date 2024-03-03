d11724d709108c399816fb15346632a48c401999
ChristianPachecoLopez <104578120+ChristianPachecoLopez@users.noreply.github.com>
2024-03-03 21:52:51 +0100

    update

---
+++ tutorial26_ExploringEventModifiers/app.js
@@ -0,0 +1,30 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: "",
+      confirmedName: "",
+    };
+  },
+  methods: {
+    confirmedInput() {
+      this.confirmedName = this.name;
+    },
+    // submitForm(event) {
+    // event.preventDefault();
+    submitForm() {
+      alert("Submitted!");
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
+    add(number) {
+      this.counter = this.counter + number;
+    },
+    remove(number) {
+      this.counter = this.counter - number;
+    },
+  },
+});
+
+app.mount("#events");
