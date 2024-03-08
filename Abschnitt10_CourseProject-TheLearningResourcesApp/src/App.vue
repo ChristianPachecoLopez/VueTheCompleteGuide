dd41291e3377f7321e6bd1e862c10f799945f5b5
ChristianPachecoLopez <104578120+ChristianPachecoLopez@users.noreply.github.com>
2024-03-08 16:01:02 +0100

    update

---
+++ Abschnitt10_CourseProject-TheLearningResourcesApp/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul>
+    <learning-resource
+      v-for="resource in storedResources"
+      :key="resource.id"
+    ></learning-resource>
+  </ul>
+</template>
+
+<script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
+export default {
+  components: {
+    'learning-resource': LearningResource,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          tittle: 'Official Guide',
+          description: 'The official guide to Vue.js',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          tittle: 'Google',
+          description: 'Google search engine',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ Abschnitt10_CourseProject-TheLearningResourcesApp/src/components/learning-resources/LearningResource.vue
@@ -0,0 +1,14 @@
+<template>
+  <li>
+    <div>
+      <header>
+        <h3>{{ resource.tittle }}</h3>
+        <button></button>
+      </header>
+    </div>
+    <p>{{ resource.description }}</p>
+    <a :href="resource.link" target="_blank">Go to resource</a>
+  </li>
+</template>
+
+<script></script>
