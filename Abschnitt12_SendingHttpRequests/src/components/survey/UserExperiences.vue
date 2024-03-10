8509678b0931c9af817cd5986920aa45f759215f
ChristianPachecoLopez <104578120+ChristianPachecoLopez@users.noreply.github.com>
2024-03-10 18:45:24 +0100

    update

--- Abschnitt12_SendingHttpRequests/src/components/survey/UserExperiences.vue
+++ Abschnitt12_SendingHttpRequests/src/components/survey/UserExperiences.vue
@@ -3,9 +3,16 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperience"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="isLoading">Loading ...</p>
+      <p v-else-if="!isLoading && error">{{ error }}</p>
+      <p v-else-if="!isLoading && results?.length === 0">
+        No stored experiences found.
+      </p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -21,9 +28,51 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+      error: null,
+    };
+  },
+  mounted() {
+    this.loadExperience();
+  },
+  methods: {
+    loadExperience() {
+      this.isLoading = true;
+      this.errror = null;
+      fetch(
+        'https://vue-http-demo-27e1a-default-rtdb.europe-west1.firebasedatabase.app/survey.json'
+      )
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          const results = [];
+          for (const key in data) {
+            const result = {
+              id: key,
+              name: data[key].name,
+              rating: data[key].rating,
+            };
+            results.push(result);
+          }
+          this.results = results;
+          this.isLoading = false;
+        })
+        .catch((error) => {
+          console.log(error);
+          this.isLoading = false;
+          this.errror =
+            'An error occurred while loading the submitted experiences.';
+        });
+    },
   },
 };
 </script>
