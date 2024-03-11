15891570633666ea68b14db81c265235d938d912
ChristianPachecoLopez <104578120+ChristianPachecoLopez@users.noreply.github.com>
2024-03-12 00:11:37 +0100

    update

---
+++ Abschnitt15_Vuex/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <base-container title="Vuex">
+    <TheCounter></TheCounter>
+    <FavoriteValue></FavoriteValue>
+    <button @click="addOne">Add 1</button>
+    <ChangeCounter></ChangeCounter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavoriteValue,
+  },
+  methods: {
+    addOne() {
+      // this.$store.state.counter++;
+      // this.$store.commit('increase', { value: 10 });
+      // this.$store.commit({
+      //   type: 'increase',
+      //   value: 10,
+      // });
+      this.$store.dispatch({
+        type: 'increase',
+        value: 10,
+      });
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
