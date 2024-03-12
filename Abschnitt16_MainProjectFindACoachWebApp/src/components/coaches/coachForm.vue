8a101662c1ad6dfd914c5a73a55110b4d42fc4b6
ChristianPachecoLopez <104578120+ChristianPachecoLopez@users.noreply.github.com>
2024-03-13 00:04:25 +0100

    update

---
+++ Abschnitt16_MainProjectFindACoachWebApp/src/components/coaches/coachForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <form action="">
+    <div class="form-control">
+      <label for="firstName">First Name</label>
+      <input type="text" id="firstName" v-model.trim="firstName" />
+    </div>
+    <div class="form-control">
+      <label for="lastName">Last Name</label>
+      <input type="text" id="lastName" v-model.trim="lastName" />
+    </div>
+    <div class="form-control">
+      <label for="description">Description</label>
+      <input type="text" id="description" v-model.trim="description" />
+    </div>
+    <div class="form-control">
+      <label for="rate">Hourly Rate</label>
+      <input type="number" id="rate" v-model.number="rate" />
+    </div>
+
+    <div class="form-control">
+      <h3>Areas of Experties</h3>
+      <div>
+        <input type="checkbox" id="frontend" value="frontend" v-model="areas" />
+        <label for="frontend">Frontend Development</label>
+      </div>
+      <div>
+        <input type="checkbox" id="backend" value="backend" v-model="areas" />
+        <label for="backend">Backend Development</label>
+      </div>
+      <div>
+        <input type="checkbox" id="career" value="career" v-model="areas" />
+        <label for="career">Career Advisory</label>
+      </div>
+    </div>
+    <base-button> Register</base-button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      description: '',
+      rate: null,
+      areas: [],
+    };
+  },
+  methods: {
+    submitForm(event) {
+      event.preventDefault();
+      const newCoach = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        description: this.description,
+        hourlyRate: this.rate,
+        areas: this.areas,
+      };
+      console.log(newCoach);
+    },
+  },
+};
+</script>
---
+++ Abschnitt16_MainProjectFindACoachWebApp/src/views/coaches/CoachRegistration.vue
@@ -0,0 +1,5 @@
+<template>
+  <section>
+    <h2>Register as a Coach now!</h2>
+  </section>
+</template>
